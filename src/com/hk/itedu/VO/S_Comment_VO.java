7195559a620a5d7d50c9ee45f3594ec12f9ffc8a
USER1-PC\USER <runitist@gmail.com>
2018-10-30 09:35:38 +0900

    comment vo 객체 생성

--- src/com/hk/itedu/VO/S_Comment_VO.java
+++ src/com/hk/itedu/VO/S_Comment_VO.java
@@ -1,5 +1,41 @@
 package com.hk.itedu.VO;
 
 public class S_Comment_VO {
+	private int board_no;
+	private int comment_no;
+	private String comment_content;
+	private String date;
+
+	public int getBoard_no() {
+		return board_no;
+	}
+
+	public void setBoard_no(int board_no) {
+		this.board_no = board_no;
+	}
+
+	public int getComment_no() {
+		return comment_no;
+	}
+
+	public void setComment_no(int comment_no) {
+		this.comment_no = comment_no;
+	}
+
+	public String getComment_content() {
+		return comment_content;
+	}
+
+	public void setComment_content(String comment_content) {
+		this.comment_content = comment_content;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
 
 }
